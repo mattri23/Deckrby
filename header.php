f6be3adb8a094adc37806de5a437597c62050268
Mattri <mattri.aspesi@gmail.com>
2025-12-10 13:44:25 +0100

    Added Base index file

---
+++ header.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Deckrby</title>
+</head>
+<body>
+
+<header>
+    <h1>DeckRby</h1>
+</header>
+
