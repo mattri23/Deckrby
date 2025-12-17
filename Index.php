f298746a0effc2d6be8ed9e498dc1de7ba15f1cf
Mattri <146237185+mattri23@users.noreply.github.com>
2025-12-17 14:12:23 +0100

    added files

--- Index.php
+++ Index.php
@@ -1,5 +1,14 @@
-<?php include "header.php" ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" href="Style/index.css">
+    <script src="HomePage.js" defer></script>
+</head>
+<body>
 
+    <button id="playBtn"><h1>Play</h1></button>
 
-
-<?php include "footer.php" ?>
+</body>
+</html>
--- header.php
+++ header.php
@@ -3,6 +3,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Deckrby</title>
+    <link rel="stylesheet" href="Style/Game.css">
+    <script src="Script.js" defer></script>
 </head>
 <body>
 
