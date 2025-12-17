f8f2a390825ca3d3b5a48c9d1a4d09ebd96613b6
Mattri <146237185+mattri23@users.noreply.github.com>
2025-12-17 14:37:43 +0100

    added title img
    added title image and modified homeScreen

--- Index.php
+++ Index.php
@@ -8,7 +8,13 @@
 </head>
 <body>
 
+<div>
+    <img src="Images/DeckRby%20titleIMG.png" alt="TitleScreen" id="titleIMG">
     <button id="playBtn"><h1>Play</h1></button>
+</div>
 
+<?php
+include "footer.php";
+?>
 </body>
 </html>
